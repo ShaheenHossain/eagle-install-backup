04e0eb038da68ccaaf78100c04c5816dab253d40
rapidgrps <rapidgrps@gmail.com>
2017-04-14 20:35:39 +0600

    Update eagle_install.sh

--- eagle_install.sh
+++ eagle_install.sh
@@ -106,7 +106,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install EAGLE
 #--------------------------------------------------
 echo -e "\n==== Installing EAGLE Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/rapidgrps/eagle-1.0 $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/rapidgrps/eagle-erp $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # Odoo Enterprise install!

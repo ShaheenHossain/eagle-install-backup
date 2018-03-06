240c16003f1c89ed547dd8ab1e83e4469d9b1ed0
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-03-06 11:51:42 +0600

    Update and rename eagle_install.sh to eagle_install_10.1.sh

--- eagle_install_10.1.sh
+++ eagle_install_10.1.sh
@@ -16,14 +16,14 @@
  
 ##fixed parameters
 #Eagle
-OE_USER="eagle"
+OE_USER="eagle10.1"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 #The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 #Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 #Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8069"
+OE_PORT="8070"
 #Choose the Odoo version which you want to install. For example: 10.0, 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 10.0
 OE_VERSION="10.0"

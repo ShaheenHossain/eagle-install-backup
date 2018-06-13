2df6e0ea3e50642b242443a914c8dba9bcf75b93
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-06-13 17:01:54 +0600

    Update eagle_install_1072.sh

--- eagle_install_1072.sh
+++ eagle_install_1072.sh
@@ -15,14 +15,13 @@
 ################################################################################
  
 ##fixed parameters
-#Eagle
+
 OE_USER="eagle1072"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
-#The default port where this eagle1071 instance will run under (provided you use the command -c in the terminal)
 #Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
-#Set the default eagle port (you still have to use -c /etc/odoo-server.conf for example to use this.)
+#Set the default eagle1072 port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 OE_PORT="8072"
 #Choose the Odoo version which you want to install. For example: 10.0, 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 10.0
@@ -57,7 +56,7 @@ sudo apt-get upgrade -y
 echo -e "\n---- Install PostgreSQL Server ----"
 sudo apt-get install postgresql -y
 
-echo -e "\n---- Creating the EAGLE PostgreSQL User  ----"
+echo -e "\n---- Creating the EAGLE1072 PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 
 #--------------------------------------------------
@@ -81,7 +80,7 @@ sudo apt-get install python-gevent -y
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
 if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
-  echo -e "\n---- Install wkhtml and place shortcuts on correct place for EAGLE 1072 ----"
+  echo -e "\n---- Install wkhtml and place shortcuts on correct place for EAGLE1072 ----"
   #pick up correct one from x64 & x32 versions:
   if [ "`getconf LONG_BIT`" == "64" ];then
       _url=$WKHTMLTOX_X64
@@ -96,7 +95,7 @@ else
   echo "Wkhtmltopdf isn't installed due to the choice of the user!"
 fi
 	
-echo -e "\n---- Create EAGLE system user ----"
+echo -e "\n---- Create EAGLE1072 system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1072' --group $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
@@ -108,7 +107,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 #--------------------------------------------------
 # Install EAGLE
 #--------------------------------------------------
-echo -e "\n==== Installing EAGLE 1072 Server ===="
+echo -e "\n==== Installing EAGLE ERP Server ===="
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/eagle-backup $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
@@ -156,7 +155,7 @@ sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/openerp-server --config=/et
 sudo chmod 755 $OE_HOME_EXT/start.sh
 
 #--------------------------------------------------
-# Adding Eagle1071 as a deamon (initscript)
+# Adding Eagle ERP as a deamon (initscript)
 #--------------------------------------------------
 
 echo -e "* Create init file"
@@ -171,7 +170,7 @@ cat <<EOF > ~/$OE_CONFIG
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
 # Short-Description: Enterprise Business Applications
-# Description: eagle1071 Business Applications
+# Description: eagle Business Applications
 ### END INIT INFO
 PATH=/bin:/sbin:/usr/bin
 DAEMON=$OE_HOME_EXT/odoo-bin
@@ -241,13 +240,13 @@ sudo chown root: /etc/init.d/$OE_CONFIG
 echo -e "* Change default xmlrpc port"
 sudo su root -c "echo 'xmlrpc_port = $OE_PORT' >> /etc/${OE_CONFIG}.conf"
 
-echo -e "* Start EAGLE on Startup"
+echo -e "* Start EAGLE ERP on Startup"
 sudo update-rc.d $OE_CONFIG defaults
 
-echo -e "* Starting eagle Service"
+echo -e "* Starting eagle ERP Service"
 sudo su root -c "/etc/init.d/$OE_CONFIG start"
 echo "-----------------------------------------------------------"
-echo "Done! The Eagle server is up and running. Specifications:"
+echo "Done! The Eagle ERP server is up and running. Specifications:"
 echo "Port: $OE_PORT"
 echo "User service: $OE_USER"
 echo "User PostgreSQL: $OE_USER"

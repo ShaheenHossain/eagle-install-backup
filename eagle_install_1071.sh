1d9a0526c9405740b120573bcdfddb789eea549b
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-06-13 16:30:26 +0600

    Update and rename eagle_install_10.1.sh to eagle_install_1071.sh

--- eagle_install_1071.sh
+++ eagle_install_1071.sh
@@ -16,14 +16,14 @@
  
 ##fixed parameters
 #Eagle
-OE_USER="eagle10.1"
+OE_USER="eagle1071"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
-#The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
+#The default port where this eagle1071 instance will run under (provided you use the command -c in the terminal)
 #Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 #Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8070"
+OE_PORT="8071"
 #Choose the Odoo version which you want to install. For example: 10.0, 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 10.0
 OE_VERSION="10.0"
@@ -81,7 +81,7 @@ sudo apt-get install python-gevent -y
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
 if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
-  echo -e "\n---- Install wkhtml and place shortcuts on correct place for EAGLE 1.0 ----"
+  echo -e "\n---- Install wkhtml and place shortcuts on correct place for EAGLE 1071 ----"
   #pick up correct one from x64 & x32 versions:
   if [ "`getconf LONG_BIT`" == "64" ];then
       _url=$WKHTMLTOX_X64
@@ -97,7 +97,7 @@ else
 fi
 	
 echo -e "\n---- Create EAGLE system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1071' --group $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
 
@@ -108,7 +108,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 #--------------------------------------------------
 # Install EAGLE
 #--------------------------------------------------
-echo -e "\n==== Installing EAGLE Server ===="
+echo -e "\n==== Installing EAGLE 1071 Server ===="
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/eagle-backup $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
@@ -156,7 +156,7 @@ sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/openerp-server --config=/et
 sudo chmod 755 $OE_HOME_EXT/start.sh
 
 #--------------------------------------------------
-# Adding ODOO as a deamon (initscript)
+# Adding Eagle1071 as a deamon (initscript)
 #--------------------------------------------------
 
 echo -e "* Create init file"
@@ -171,7 +171,7 @@ cat <<EOF > ~/$OE_CONFIG
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
 # Short-Description: Enterprise Business Applications
-# Description: ODOO Business Applications
+# Description: eagle1071 Business Applications
 ### END INIT INFO
 PATH=/bin:/sbin:/usr/bin
 DAEMON=$OE_HOME_EXT/odoo-bin

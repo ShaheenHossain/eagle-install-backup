7e51558adb97bf36717aa37ca7e9214753f4c802
ShaheenHossain <rapidgrps@princegroup-bd.com>
2020-07-27 02:29:02 +0600

    Update eagle-dev1335-install.sh

--- eagle-dev1335-install.sh
+++ eagle-dev1335-install.sh
@@ -38,7 +38,7 @@ echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
 
 echo -e "\n---- Install python packages/requirements ----"
-sudo pip3 install -r https://github.com/ShaheenHossain/eagle_13_01/raw/${OE_VERSION}/requirements.txt
+sudo pip3 install -r https://github.com/ShaheenHossain/eagle13.02/raw/${OE_VERSION}/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
@@ -76,7 +76,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install Eagle
 #--------------------------------------------------
 echo -e "\n==== Installing Eagle13 Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/eagle_13_01 $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/eagle13.02 $OE_HOME_EXT/
 
 #Enterprise deleted
 

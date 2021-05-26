0794430b5ea88fe80d99e543d59d5c312030032a
ShaheenHossain <rapidgrps@princegroup-bd.com>
2021-05-26 20:11:55 +0600

    Update eagle-ent1455-install.sh

--- eagle-ent1455-install.sh
+++ eagle-ent1455-install.sh
@@ -99,7 +99,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install EAGLE
 #--------------------------------------------------
 echo -e "\n==== Installing EAGLE Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/odoo14_ent_unlimit $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/eagle14ent_01 $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # EAGLE Enterprise install!

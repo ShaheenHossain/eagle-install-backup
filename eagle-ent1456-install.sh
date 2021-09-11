cad9f6fe09821f60a47fc926bf2822ac69611449
ShaheenHossain <rapidgrps@princegroup-bd.com>
2021-09-11 17:14:39 +0600

    Update eagle-ent1456-install.sh

--- eagle-ent1456-install.sh
+++ eagle-ent1456-install.sh
@@ -1,9 +1,9 @@
-OE_USER="eagle1456"
+OE_USER="eagle1457"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 INSTALL_WKHTMLTOPDF="True"
 
-OE_PORT="8056"
+OE_PORT="8057"
 # IMPORTANT! This script contains extra libraries that are specifically needed for Eagle 14.0
 OE_VERSION="master"
 
@@ -87,7 +87,7 @@ else
 fi
 
 echo -e "\n---- Create EAGLE system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1456' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1457' --group $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
 

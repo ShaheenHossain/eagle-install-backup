70691ab4e8a60040f27338ed851e4280f7339eb3
ShaheenHossain <rapidgrps@princegroup-bd.com>
2021-12-22 04:13:28 +0600

    Update eagle-cntos1561-install.sh

--- eagle-cntos1561-install.sh
+++ eagle-cntos1561-install.sh
@@ -68,6 +68,8 @@ sudo useradd -m -U -r -d $OE_HOME -s /bin/bash $OE_USER
 
 sudo yum install wkhtmltopdf -y
 
+#sudo yum -y install https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.centos7.x86_64.rpm
+
 sudo mkdir /var/log/$OE_USER
 
 

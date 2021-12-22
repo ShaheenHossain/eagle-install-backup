225d0dd7a39f622d6f8d31ae5b426bebdf3da448
ShaheenHossain <rapidgrps@princegroup-bd.com>
2021-12-22 11:07:01 +0600

    Update eagle-cntos1561-install.sh

--- eagle-cntos1561-install.sh
+++ eagle-cntos1561-install.sh
@@ -4,26 +4,14 @@ OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 INSTALL_WKHTMLTOPDF="True"
 
 OE_PORT="8061"
-# IMPORTANT! This script contains extra libraries that are specifically needed for Eagle 15.0
 OE_VERSION="15.0"
 
 OE_SUPERADMIN="admin"
-# Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
 OE_CONFIG="${OE_USER}-server"
-# Set the website name
 WEBSITE_NAME="_"
 LONGPOLLING_PORT="8072"
-# Provide Email to register ssl certificate
 ADMIN_EMAIL="rapidgrps@gmail.com"
 
-#sudo yum -y install https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.centos7.x86_64.rpm
-
-#WKHTMLTOX_X64=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.centos7.x86_64.rpm
-#WKHTMLTOX_X32=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.centos7.i386.deb
-#--------------------------------------------------
-# Update Server
-#--------------------------------------------------
-echo -e "\n---- Update Server ----"
 sudo yum -y install epel-release
 
 # libpng12-0 dependency for wkhtmltopdf
@@ -39,14 +27,14 @@ sudo yum install python-pip -y
 sudo pip install --upgrade setuptools
 sudo pip install Babel decorator docutils ebaysdk feedparser gevent greenlet jcconv Jinja2 lxml Mako MarkupSafe mock ofxparse passlib Pillow psutil psycogreen psycopg2-binary pydot pyparsing pyPdf pyserial Python-Chart python-dateutil python-ldap python-openid pytz pyusb PyYAML qrcode reportlab requests six suds-jurko vatnumber vobject Werkzeug wsgiref XlsxWriter xlwt xlrd
 sudo pip install -r https://raw.githubusercontent.com/odoo/odoo/15.0/requirements.txt
-sudo yum install python39 -y
+sudo yum install python37 -y
 
-sudo yum install python39 python39-devel
+sudo yum install python37 python37-devel
 
 sudo yum -y install git gcc wget nodejs libxslt-devel bzip2-devel openldap-devel libjpeg-devel freetype-devel
 
-sudo yum install python39-devel libxslt-devel libxml2-devel openldap-devel python39-setuptools python-devel -y
-python3.6 -m ensurepip
+sudo yum install python37-devel libxslt-devel libxml2-devel openldap-devel python37-setuptools python-devel -y
+python3.7 -m ensurepip
 sudo pip3 install pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2-binary psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd num2words
 sudo pip3 install -r https://raw.githubusercontent.com/odoo/odoo/15.0/requirements.txt
 
@@ -99,59 +87,109 @@ sudo su root -c "echo '#!/bin/sh' >> $OE_HOME_EXT/start.sh"
 sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/odoo-bin --config=/etc/${OE_CONFIG}.conf' >> $OE_HOME_EXT/start.sh"
 sudo chmod 755 $OE_HOME_EXT/start.sh
 
-#sudo su root -c "touch '$OE_CONFIG'"
 
-#sudo su root -c "echo "[options]" >> $OE_CONFIG"
-#sudo su root -c "echo ';This is the password that allows database operations:' >> $OE_CONFIG"
-#sudo su root -c "echo 'admin_passwd = $OE_MASTER_PASSWD' >> $OE_CONFIG"
-#sudo su root -c "echo 'xmlrpc_port = $OE_PORT' >> $OE_CONFIG"
-#sudo su root -c "echo 'logfile = /var/log/$OE_USER/$OE_USER.log' >> $OE_CONFIG"
-#sudo chmod 640 $OE_CONFIG
+cat <<EOF > ~/$OE_CONFIG
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: $OE_CONFIG
+# Required-Start: \$remote_fs \$syslog
+# Required-Stop: \$remote_fs \$syslog
+# Should-Start: \$network
+# Should-Stop: \$network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Enterprise Business Applications
+# Description: Eagle Business Applications
+### END INIT INFO
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+DAEMON=$OE_HOME_EXT/odoo-bin
+NAME=$OE_CONFIG
+DESC=$OE_CONFIG
+# Specify the user name (Default: odoo).
+USER=$OE_USER
+# Specify an alternate config file (Default: /etc/openerp-server.conf).
+CONFIGFILE="/etc/${OE_CONFIG}.conf"
+# pidfile
+PIDFILE=/var/run/\${NAME}.pid
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS="-c \$CONFIGFILE"
+[ -x \$DAEMON ] || exit 0
+[ -f \$CONFIGFILE ] || exit 0
+checkpid() {
+[ -f \$PIDFILE ] || return 1
+pid=\`cat \$PIDFILE\`
+[ -d /proc/\$pid ] && return 0
+return 1
+}
+case "\${1}" in
+start)
+echo -n "Starting \${DESC}: "
+start-stop-daemon --start --quiet --pidfile \$PIDFILE \
+--chuid \$USER --background --make-pidfile \
+--exec \$DAEMON -- \$DAEMON_OPTS
+echo "\${NAME}."
+;;
+stop)
+echo -n "Stopping \${DESC}: "
+start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
+--oknodo
+echo "\${NAME}."
+;;
+restart|force-reload)
+echo -n "Restarting \${DESC}: "
+start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
+--oknodo
+sleep 1
+start-stop-daemon --start --quiet --pidfile \$PIDFILE \
+--chuid \$USER --background --make-pidfile \
+--exec \$DAEMON -- \$DAEMON_OPTS
+echo "\${NAME}."
+;;
+*)
+N=/etc/init.d/\$NAME
+echo "Usage: \$NAME {start|stop|restart|force-reload}" >&2
+exit 1
+;;
+esac
+exit 0
+EOF
+
+echo -e "* Security Init File"
+sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
+sudo chmod 755 /etc/init.d/$OE_CONFIG
+sudo chown root: /etc/init.d/$OE_CONFIG
+
+echo -e "* Start EAGLE on Startup"
+sudo update-rc.d $OE_CONFIG defaults
 
 
 
-echo -e "\n---- Creating systemd config file"
-sudo touch /etc/systemd/system/$OE_USER.service
-
-sudo su root -c "echo "[Unit]" >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'Description=Odoo server' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo '#Requires=postgresql-9.6.service' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo '#After=network.target postgresql-9.6.service' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo "[Service]" >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'Type=simple' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'SyslogIdentifier=odoo15' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'PermissionsStartOnly=true' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'User=$OE_USER' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'Group=$OE_USER' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'ExecStart=$OE_HOME/$OE_USER/odoo-bin -c /etc/$OE_USER.conf' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'StandardOutput=journal+console' >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo "[Install]" >> /etc/systemd/system/$OE_USER.service"
-sudo su root -c "echo 'WantedBy=multi-user.target' >> /etc/systemd/system/$OE_USER.service"
-
-
-echo -e "\n---- Start OE on Startup"
-sudo chmod +x /etc/systemd/system/$OE_USER.service
-sudo systemctl daemon-reload
-
-sudo chown -R $OE_USER: $OE_HOME
-sudo chown $OE_USER: $OE_CONFIG
-
-echo -e "\n---- Starting Odoo Service"
-
-sudo systemctl start $OE_USER.service
-sudo systemctl enable $OE_USER.service
-sudo systemctl status $OE_USER.service
-
+echo -e "* Starting Eagle Service"
+sudo su root -c "/etc/init.d/$OE_CONFIG start"
 echo "-----------------------------------------------------------"
-echo "Done! The Odoo server is up and running. Specifications:"
-echo "-----------------------------------------------------------"
+echo "Done! The Eagle server is up and running. Specifications:"
 echo "Port: $OE_PORT"
-echo "Master password: $OE_MASTER_PASSWD"
 echo "User service: $OE_USER"
+echo "Configuraton file location: /etc/${OE_CONFIG}.conf"
+echo "Logfile location: /var/log/$OE_USER"
 echo "User PostgreSQL: $OE_USER"
-echo "Addons folder: $OE_HOME_EXT/addons and $OE_HOME/custom/addons"
-echo "Start Odoo service: service $OE_USER start"
-echo "Stop Odoo service: service $OE_USER stop"
-echo "Restart Odoo service: service $OE_USER restart"
+echo "Code location: $OE_USER"
+echo "Addons folder: $OE_USER/$OE_CONFIG/odoo/addons/"
+echo "Password superadmin (database): $OE_SUPERADMIN"
+echo "Start Eagle service: sudo service $OE_CONFIG start"
+echo "Stop Eagle service: sudo service $OE_CONFIG stop"
+echo "Restart Eagle service: sudo service $OE_CONFIG restart"
+echo "-----------------------------------------------------------"
+
+
+
+
+
+
+
+
+
+
+
 
 

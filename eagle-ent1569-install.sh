836eefbac68c2f466ca385311c0392ea30e6f2e7
ShaheenHossain <rapidgrps@princegroup-bd.com>
2022-03-12 16:46:28 +0600

    Update eagle-ent1569-install.sh

--- eagle-ent1569-install.sh
+++ eagle-ent1569-install.sh
@@ -96,7 +96,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install EAGLE
 #--------------------------------------------------
 echo -e "\n==== Installing EAGLE Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/marko53_mbp_15ent $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/drazank_holli_15ent $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # EAGLE Enterprise install!

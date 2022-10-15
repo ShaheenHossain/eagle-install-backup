5aca6b6221534bfa1e3376caa6e4b33857a71eaf
ShaheenHossain <rapidgrps@princegroup-bd.com>
2022-10-15 21:01:11 +0600

    Update eagle-ent1651-install.sh

--- eagle-ent1651-install.sh
+++ eagle-ent1651-install.sh
@@ -79,7 +79,7 @@ else
 fi
 
 echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1651' --group $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
 

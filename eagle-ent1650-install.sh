6dc04895b5148d3ddc85748bc2e50dfc54c7eecb
ShaheenHossain <rapidgrps@princegroup-bd.com>
2022-10-15 20:13:12 +0600

    Update eagle-ent1650-install.sh

--- eagle-ent1650-install.sh
+++ eagle-ent1650-install.sh
@@ -8,7 +8,7 @@ OE_PORT="1650"
 # IMPORTANT! This script contains extra libraries that are specifically needed for Eagle 15.0
 OE_VERSION="16.0"
 
-IS_ENTERPRISE="True"
+IS_ENTERPRISE="False"
 
 INSTALL_NGINX="Flase"
 

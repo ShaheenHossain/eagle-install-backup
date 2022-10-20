6260be708982bfbe7e631b9fc8cc2d4e6756f31c
ShaheenHossain <rapidgrps@princegroup-bd.com>
2022-10-20 17:22:24 +0600

    Update eagle-ent1655-install.sh

--- eagle-ent1655-install.sh
+++ eagle-ent1655-install.sh
@@ -1,11 +1,11 @@
-E_USER="eagle1656"
+E_USER="eagle1655"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 # The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 # Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 # Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8056"
+OE_PORT="8055"
 # Choose the Odoo version which you want to install. For example: 16.0, 15.0, 14.0 or saas-22. When using 'master' the master version will be installed.
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 16.0
 OE_VERSION="master"
